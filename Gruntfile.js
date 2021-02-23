da09ba4273cfb5c152d222e4202bad2cda7f1739
Carlos Gomes <carlos.algms@gmail.com>
2021-02-23 23:06:28 +0100

    Removed Grunt build for JavaScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,39 +26,6 @@ module.exports = (grunt) => {
     clean: {
       fontawesome: ['source/css/fonts'],
       materialize: ['source/font'],
-      jsCompiled: ['source/js'],
-    },
-
-    terser: {
-      options: {
-        mangle: false,
-        ecma: 2018,
-        sourceMap: false,
-      },
-      vendors: {
-        files: {
-          '.tmp/vendors.min.js': [
-            'node_modules/jquery-circle-progress/dist/circle-progress.js',
-            'node_modules/requirejs/require.js',
-          ],
-        },
-      },
-      jsSources: {
-        expand: true,
-        src: ['source/js/**/*.js'],
-        dest: './',
-      },
-    },
-
-    concat: {
-      scripts: {
-        dest: 'source/js/vendors.js',
-        src: [
-          'node_modules/jquery/dist/jquery.min.js',
-          'node_modules/materialize-css/dist/js/materialize.min.js',
-          '.tmp/vendors.min.js',
-        ],
-      },
     },
 
     cssmin: {
@@ -73,39 +40,10 @@ module.exports = (grunt) => {
         },
       },
     },
-
-    requirejs: {
-      all: {
-        options: {
-          waitSeconds: 0,
-          appDir: 'src/scripts',
-          mainConfigFile: 'src/scripts/app.js',
-          baseUrl: '.',
-          optimize: 'none',
-          dir: 'source/js',
-          generateSourceMaps: false,
-          preserveLicenseComments: true,
-          inlineText: true,
-          findNestedDependencies: true,
-          paths: {
-            jquery: 'empty:',
-            // lightbox need to be here, because it cant find images if placed on head
-            lightbox: '../../node_modules/lightbox2/dist/js/lightbox',
-          },
-          modules: [
-            {
-              name: 'app',
-            },
-          ],
-          shim: {},
-          writeBuildTxt: false,
-        },
-      },
-    },
   });
 
   loadGruntTasks(grunt);
 
   grunt.registerTask('default', []);
-  grunt.registerTask('build', ['clean', 'copy', 'cssmin', 'requirejs', 'terser', 'concat']);
+  grunt.registerTask('build', ['clean', 'copy', 'cssmin']);
 };
